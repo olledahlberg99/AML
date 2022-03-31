3ad85b31d5abe027071aa3e2c87d00f3a51ca4fd
olledahlberg99 <77774782+olledahlberg99@users.noreply.github.com>
2022-03-31 14:18:33 +0200

    Add files via upload

--- agentClass.py
+++ agentClass.py
@@ -2,7 +2,13 @@ import numpy as np
 import random
 import math
 import h5py
-import itertools
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+
 
 def binatodeci(binary):
     deci = sum(val*(2**idx) for idx, val in enumerate(reversed(binary)))
@@ -115,6 +121,24 @@ class TQAgent:
             # Update the Q-table using the old state and the reward (the new state and the taken action should be stored as attributes in self)
             self.fn_reinforce(old_state,reward)
 
+class DQN(nn.Module):
+
+    def __init__(self, h, w, outputs):
+        super(DQN, self).__init__()
+        self.
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+
+    def forward(self, x):
+        x = x.to(device)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
 
 class TDQNAgent:
     # Agent for learning to play tetris using Q-learning
